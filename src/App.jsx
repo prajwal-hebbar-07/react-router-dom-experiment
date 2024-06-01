fa4d7ce3df2af24a5cf2d5c605d8a3502aa68c46
Hebbar <prajwalhebbaras@gmail.com>
2024-06-01 10:36:18 +0530

    Added nav styles and usage of old approach to declare routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // Import for all the items required to add routes for the application.
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 
 // Import for all the pages in the routes.
 import Home from "./pages/Home";
@@ -8,9 +8,16 @@ import About from "./pages/About";
 const App = () => {
   return (
     <BrowserRouter>
+      <header>
+        <nav>
+          <h1>React Router DOM Experiment</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
         </Routes>
       </main>
