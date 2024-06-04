e3f33a25cd1e1cf52e30809a22d1b07a654a90b7
Hebbar <prajwalhebbaras@gmail.com>
2024-06-04 11:00:57 +0530

    form component from react router

--- src/pages/help/Contact.jsx
+++ src/pages/help/Contact.jsx
@@ -1,9 +1,13 @@
+import { Form, redirect, useActionData } from "react-router-dom";
+
 const Contact = () => {
+  const data = useActionData();
+
   return (
     <div className="contact">
       <h3>Contact Us Section</h3>
 
-      <form>
+      <Form method="post" action="/help/contact">
         <label>
           <span>Email: </span>
           <input type="email" name="email" required />
@@ -13,9 +17,28 @@ const Contact = () => {
           <textarea name="message" required></textarea>
         </label>
         <button>Submit</button>
-      </form>
+
+        {data && data.error && <p>{data.error}</p>}
+      </Form>
     </div>
   );
 };
 
 export default Contact;
+
+export const contactAction = async ({ request }) => {
+  const data = await request.formData();
+  const submissionData = {
+    email: data.get("email"),
+    message: data.get("message"),
+  };
+
+  console.log(submissionData);
+
+  // Error message display
+  if (submissionData.message.length < 10) {
+    return { error: "The message should be atleast 10 characters long" };
+  }
+  // redirect
+  return redirect("/");
+};
